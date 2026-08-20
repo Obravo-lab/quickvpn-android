4a0e092cc708c39397424434d8279a25f60ebb7d
QuickVPN <contact@quickvpn.fr>
2026-08-20 17:51:59 +0200

    versionCode 13 / 1.4.4 (clean final build)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "fr.quickvpn.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.4.3"
+        versionCode = 13
+        versionName = "1.4.4"
     }
 
     signingConfigs {
